30bae2ba430b81cdb5d614bd3bf7954bfd8cca76
Patricia Adoga <patriciaadoga@gmail.com>
2020-06-28 13:25:51 +0100

    fixes to server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 
-app.use(express.static("dist"));
+const app = express()
 
-app.listen(3000, function() {
-  console.log("Ticket app listening on port 3000!");
-});
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`ticket app is listening on port: ${port}`)
+})
